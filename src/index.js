aeb26708146adf8c6a71655ef72ba75b9ea1c271
himanshu-adcat <himanshus45@hdfclife.com>
2019-08-24 13:26:57 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import 'Styles/style.scss'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Login from './views/login/'
+import FormContainer from './views/'
 
 class App extends Component {
   render () {
     return (
       <React.Fragment>
-        <Router>
-          <Route exact path='/' component={Login} />
-        </Router>
+        <FormContainer />
       </React.Fragment>
     )
   }
